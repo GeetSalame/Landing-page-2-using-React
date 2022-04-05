1fa8c84f47f8c1a1f1a1230ba4e46e48dc3f56b3
GeetSalame <geetsalame156@gmail.com>
2022-04-05 21:32:01 +0530

    some design changes

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import fb from "../images/facebook.png";
 import instagram from "../images/instagram.png";
 import twitter from "../images/twitter.png";
+import mail from "../images/mail.png";
 function Connect() {
     return (
         <div id="connect">
             <h2>Connect</h2>
             <div className="socialmedia">
+                <img src={mail} alt="" />
                 <img src={fb} alt="" />
                 <img src={instagram} alt="" />
                 <img src={twitter} alt="" />
